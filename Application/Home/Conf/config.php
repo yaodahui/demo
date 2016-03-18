107e6a88e07b62b1792be7fbfd34e42087ee4ace
yaohui <353702232@qq.com>
2016-03-18 15:43:19 +0800

    first commit

---
+++ Application/Home/Conf/config.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+	'name'=>'kijjhj1',
+	//'配置项'=>'配置值',
+	'URL_MODEL'=>2,
+	'URL_HTML_SUFFIX'=>'shtml|html|xml',
+);
---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,20 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<?php
+ echo $name2; ?>
+<br/>
+<?php
+ echo $name; ?>
+<br/>
+<?php
+ echo $sex; ?>
+<br/>
+<?php
+ echo $today; ?>
+</body>
+</html>
